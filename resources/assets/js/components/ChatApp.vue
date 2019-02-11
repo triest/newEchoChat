181f08b5bd62c587ff62ab37e1dacdf1f61f37b2
triest <triest21@gmail.com>
2019-02-11 23:27:55 +0300

    First commit

---
+++ resources/assets/js/components/ChatApp.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="chat-app">
+        <Conversation :contact="selectedContact" :messages="messages" @new="saveNewMessage"/>
+        <ContactsList :contacts="contacts" @selected="startConversationWith"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                selectedContact: null,
+                messages: [],
+                contacts: []
+            };
+        },
+        mounted() {
+            Echo.private(`messages.${this.user.id}`)
+                .listen('NewMessage', (e) => {
+                    this.hanleIncoming(e.message);
+                });
+            axios.get('/contacts')
+                .then((response) => {
+                    this.contacts = response.data;
+                });
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
